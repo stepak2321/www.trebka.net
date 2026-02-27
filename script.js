d45ea58606712cb04336f5c9e570c4999588fcd0
stepak2321 <stepank.2321@gmail.com>
2026-02-27 20:57:02 +0100

    Add 'balls' command to redirect in executeTerminalCommand function

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-// Function to execute commands
 function executeTerminalCommand(command) {
-    switch (command) {
-        case 'balls':
-            console.log('Redirecting to c.trebka.net');
-            window.location.href = 'http://c.trebka.net'; // Redirecting to the website
-            break;
-        // other cases
-        default:
-            console.log('Unknown command');
+    if (command === 'balls') {
+        window.location.href = 'https://c.trebka.net';
     }
+    // Original content goes here
 }
